e988952b29fab6a63db8b832a708bd89b491c9fb
Jason Payne <j.payne.1976@gmail.com>
2021-03-22 19:53:49 +0000

    Create a list of the GTFS table data

--- ingestRawData/ingestGtfs.R
+++ ingestRawData/ingestGtfs.R
@@ -23,3 +23,53 @@ download.file(
 unzip(
   'C:/MyApps/dapTbElectricDublinBus/ingestRawData/gtfs.zip',
   exdir = 'ingestRawData/raw')
+
+
+# GTFS File Descriptions
+# ======================
+# Ref.:
+# https://developers.google.com/transit/gtfs/reference
+
+# agency.txt 
+# Transit agencies with service represented in this dataset.
+ 
+# stops.txt
+# Stops where vehicles pick up or drop off riders. Also defines stations and station entrances.
+ 
+# routes.txt	
+# Required	Transit routes. A route is a group of trips that are displayed to riders as a single service.
+ 
+# trips.txt	
+# Trips for each route. A trip is a sequence of two or more stops that occur during a specific time period.
+ 
+# stop_times.txt
+# Required	Times that a vehicle arrives at and departs from stops for each trip.
+ 
+# calendar.txt	
+# Conditionally required	Service dates specified using a weekly schedule with start and end dates. 
+# This file is required unless all dates of service are defined in calendar_dates.txt.
+ 
+# calendar_dates.txt	
+# Conditionally required	Exceptions for the services defined in the calendar.txt.
+# If calendar.txt is omitted, then calendar_dates.txt is required and must contain all dates of service.
+
+
+# Create an emtpy list to hold the GTFS data
+data <- list()
+
+# Get a vector of file names that have been unzipped
+files <- list.files('ingestRawData/raw/')
+
+# Loop through and add each file to the list
+for (file in 1:length(files)){
+  file <- files[file]
+  table_name <- gsub('.txt', '', file)
+  data_add <- read.table(paste0('ingestRawData/raw/', file), sep = ',', header = TRUE)
+  names(data_add) <- gsub('ï..', '', names(data_add))
+  data[[table_name]] <- data_add  
+}
+
+
+
+
+

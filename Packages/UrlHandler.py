e2ba759ee3f1a7ef4162cae3d1a746f67b13e423
James Cookland <x20167814@student.ncirl.ie>
2021-03-22 22:47:46 +0000

    Initial Commit from James C

---
+++ Packages/UrlHandler.py
@@ -0,0 +1,14 @@
+from typing import ClassVar
+
+
+import urllib
+
+class UrlHandler(object):
+    def __init__(self, *args):
+        self.arg = "Put args here"
+    def __call__(self, callargs):
+        if callargs == "Call URL":
+            "do something in the URL"
+    
+    def callURL(self):
+        return "call URL"

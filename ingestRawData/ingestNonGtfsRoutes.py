9916474581844878f030e3afdb5ee191aa02d17f
Jason Payne <j.payne.1976@gmail.com>
2021-03-25 07:42:29 +0000

    Basic communication established with Azure maps for route selection

--- ingestRawData/ingestNonGtfsRoutes.py
+++ ingestRawData/ingestNonGtfsRoutes.py
@@ -1,47 +1,48 @@
-# th
+# Azure Maps API Interaction
+# ========================== 
+# Create an ingestion script for pulling route info from Azure maps
+# based on provided lat and long coordinates for the start and end
+# journey points.
 
-# Function to query time and distance to travel between the two stops
-def get_time_and_dist(begin: Stop, end: Stop):
-    
-    # First search db to find if values already exists.
-    tc = TravelCache.objects.filter(from_stop=begin).filter(to_stop=end)
-    if len(tc) > 0:
-        return tc[0]
+# This is required for finding 'dead head' route info, e.g., 
+# travel by the bus from the depot to the block starting point, 
+# and back again in the evening.
+# Also applicable to certain blocks where the buss will nee to travel between
+# the end and starting point of two successive trips.
 
-    # Otherwise query azure maps API if values not found in DB.
-    else:
-        # The Synchronous API will return a timeout error (a 408 response) if the request takes longer than 60 seconds.
-        # The number of batch items is limited to 100 for this API.
-        route_api_url = 'https://atlas.microsoft.com/route/directions/batch/sync/json?api-version=' + API_VERSION \
-                    + '&subscription-key=' + SUBSCRIPTION_KEY + '&traffic=' + CHECK_TRAFFIC
-        query_item = '?query={0},{1}:{2},{3}'.format(begin.stop_lat, begin.stop_lon, end.stop_lat, end.stop_lon)
-        payload = {'batchItems': [{'query': query_item}]}
-        headers = {'Content-Type': 'application/json'}
-        response = requests.request("POST", route_api_url, headers=headers, data=json.dumps(payload))
-        response_data = json.loads(response.text)
-        if 'batchItems' in response_data:
-            items = response_data['batchItems']
-            # for item returned
-            #   find time and distance from summary and store in TravelCache
-            #   also find the lat/lon points of the route and store in TravelCacheShapes
-            for i in items:
-                if(i['statusCode'] == 200):
-                    summary = i['response']['routes'][0]['summary']
+import requests
+import json
 
-                    # create new cache entry and save to db
-                    tc = TravelCache(from_stop=begin,
-                                     to_stop=end,
-                                     dist_in_meter=summary['lengthInMeters'],
-                                     time_in_sec=summary['travelTimeInSeconds'])
+filepath = 'keys.json'
+with open(filepath) as json_file:
+    key_data = json.load(json_file)
+    subscription_key = key_data['subscription_key']  
+    print(subscription_key)
 
-                    tc.save()
+api_version = '1.0'
+check_traffic = '0'
 
-                    # save the lat/long of this route in TravelCacheShape
-                    points = i['response']['routes'][0]['legs'][0]['points']
-                    tcs = [TravelCacheShape(travel_cache=tc, shape_pt_lat=p['latitude'], shape_pt_lon=p['longitude'], shape_pt_sequence=i+1) for i, p in enumerate(points)]
-                    TravelCacheShape.objects.bulk_create(tcs)
-                    return tc
-                else:
-                    return None
-        else:
-            return None
+begin = {'stop_lat':53.116927776990494, 'stop_lon':-7.325474118853148}
+end = {'stop_lat':53.03361922891958, 'stop_lon':-7.303225998037869} 
+
+# Azure Maps route URL (don't check traffic for this simple use-case)
+# Use batch mode so that the same query can be used for multiple route requests
+route_api_url = 'https://atlas.microsoft.com/route/directions/batch/sync/json?api-version=' + api_version \
+                    + '&subscription-key=' + subscription_key + '&traffic=' + check_traffic
+
+# Create query string for start and end coordinates                    
+query_item = '?query={0},{1}:{2},{3}'.format(begin['stop_lat'], begin['stop_lon'], end['stop_lat'], end['stop_lon'])
+payload = {'batchItems': [{'query': query_item}]}
+headers = {'Content-Type': 'application/json'}
+
+# Request the response from Azure maps
+response = requests.request("POST", route_api_url, headers=headers, data=json.dumps(payload))
+
+# checking the status code of the request
+if response.status_code == 200:
+   # Convert json to python dictionary
+   response_data = json.loads(response.text)
+   print("Successful HTTP request\n")
+   print(response_data)
+else:   
+   print("Error in the HTTP request")

ea2f8708f1ac33c6a93f8b31f2fcabf70093f9d0
Jason Payne <j.payne.1976@gmail.com>
2021-03-28 13:40:57 +0100

    Create code to write pandas dataframe to SQL database table

--- createNewData/extractTransformLoadRoutes.py
+++ createNewData/extractTransformLoadRoutes.py
@@ -6,8 +6,8 @@ import pandas as pd
 filepath = 'C:\MyApps\dapTbElectricDublinBus\keys.json'
 with open(filepath) as json_file:
     key_data = json.load(json_file)
-    cosmos_key = key_data['cosmos_key']  
-    print(cosmos_key)
+    cosmos_key = key_data['cosmos_key'] 
+    sqldb_pwd = key_data['sqldb_pwd'] 
 
 #%%    
 # Extract Data Raw Data from Azure Cosmos DB for MongoDB API
@@ -45,7 +45,7 @@ route_points = raw_data['batchItems'][0]['response']['routes'][0]['legs'][0]['po
 summary_info = raw_data['batchItems'][0]['response']['routes'][0]['summary']
 sections_info = raw_data['batchItems'][0]['response']['routes'][0]['sections'][0]
 
-# Now loop through and append data to the lists
+# Now loop through and append data to the appropriate lists
 for point in route_points :
     non_route_id.append(1)
     latitude.append(point['latitude'])
@@ -54,8 +54,46 @@ for point in route_points :
     time_hrs.append(round(summary_info['travelTimeInSeconds'] / (60*60), 6))
     mode.append(sections_info['travelMode'])
     
+# Create a dataframe & populate with the list data    
 route_df = pd.DataFrame(non_route_id, columns = ['non_route_id'])
 route_df['latitude'] = latitude
 route_df['longitude'] = longitude
 route_df['distance_km'] = distance_km
 route_df['time_hrs'] = time_hrs
+
+#%%
+# Create a connection to the Azure SQL database
+import pyodbc
+ 
+server = 'tcp:electricbus.database.windows.net'
+database = 'electricbus-eastus-prod'
+username = 'teamadmin'
+password = sqldb_pwd
+ 
+connection_string = 'DRIVER={ODBC Driver 13 for SQL Server};SERVER=' + server + \
+    ';DATABASE=' + database+';UID=' + username + ';PWD=' + password
+    
+conn = pyodbc.connect(connection_string)
+#cursor = conn.cursor()
+
+# Test the connection
+query = 'SELECT * FROM bus_routes'   
+test = pd.read_sql_query(query, conn)
+
+#%%
+# Create code to write a pandas dataframe to SQL table
+# Using a similar method to dbWriteTable in R
+from sqlalchemy import create_engine
+from urllib.parse import quote_plus
+import time
+
+quoted = quote_plus(connection_string)
+new_con = 'mssql+pyodbc:///?odbc_connect={}'.format(quoted)
+engine = create_engine(new_con,  fast_executemany = True)
+
+table_name = 'nongtfs_routes'
+df = route_df
+
+s = time.time()
+df.to_sql(table_name, engine, if_exists = 'replace', chunksize = None, index = False)
+print('Time taken: ' + str(round(time.time() - s, 1)) + 's')

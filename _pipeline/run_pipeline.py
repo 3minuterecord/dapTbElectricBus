097adfe29472135ae76de06f383e8897263752ec
Jason Payne <j.payne.1976@gmail.com>
2021-04-23 15:45:21 +0100

    Add basic error handling to pipeline script

--- _pipeline/run_pipeline.py
+++ _pipeline/run_pipeline.py
@@ -1,66 +1,67 @@
 import subprocess
 import os
 
-# TODO -- Catch the exception (WindowsError, OSError) on invalid path. 
-# If the exception is thrown, do not perform any recursive operations, 
-# especially destructive ones.
-os.chdir('C:\MyApps\dapTbElectricDublinBus\_pipeline')
-rscript_command ='C:/Program Files/R/R-4.0.2/bin/Rscript'
 pipeline_dir = 'C:/MyApps/dapTbElectricDublinBus/_pipeline/'
+rscript_command ='C:/Program Files/R/R-4.0.2/bin/Rscript'
 
-#%%
-# STEP 1 - INGEST RAW GTFS DATA & SAVE TO AZURE SQL DB
-# ====================================================
-# Build & run subprocess command
-# WARNING --- Saving the data to the DB takes sevral hours.
-script ='ingestGtfs.R'
-cmd = [rscript_command, pipeline_dir + script]
-subprocess.run(cmd)
-
-#%%
-# STEP 2 - CREATE BLOCKS, DEAD TRIP & DEAD LEG INFO
-# =================================================
-# Build & run subprocess command
-# WARNING --- This processing script takes sevral hours to run.
-script ='createBlockInfo.R'
-cmd = [rscript_command, pipeline_dir + script]
-subprocess.run(cmd)
-
-#%%
-# STEP 3 - GET RAW ROUTE INFO FOR DEAD LEGS & DEAD TRIPS
-# ======================================================
-import ingestNonGtfsRoutes
-
-def service_func_ingr():
-    print('Running ingest non-GTFS route script...')
-
-if __name__ == '__main__':
-    # if this scipt is executed as script, run:
-    service_func_ingr()
-    ingestNonGtfsRoutes.run_all_ingr()
-
-
-#%%
-# STEP 4 - EXTRACT RAW ROUTE DATA & TRANSFORM
-# ======================================================
-import extractTransformLoadRoutes
-
-def service_func_etlr():
-    print('Running extract & transfrom non-GTFS script...')
-
-if __name__ == '__main__':
-    # if this scipt is executed as script, run:
-    service_func_etlr()
-    extractTransformLoadRoutes.run_all_etlr()
-
-#%%
-# STEP 5 - CREATE NETWORK SUMMARY INFO
-# ====================================
-# Build & run subprocess command
-# WARNING --- This processing script takes sevral hours to run.
-script ='createBlockSummary.R'
-cmd = [rscript_command, pipeline_dir + script]
-subprocess.run(cmd)
-
-#%%
+try:
+    os.chdir(pipeline_dir)    
+# Catch invalid path    
+except WindowsError as e:
+   print("Error:" + str(e))
+# Catch file not found error    
+except OSError as e:    
+   print("Error:" + str(e))
+else :    
+    # STEP 1 - INGEST RAW GTFS DATA & SAVE TO AZURE SQL DB
+    # ====================================================
+    # Build & run subprocess command
+    # WARNING --- Saving the data to the DB takes sevral hours.
+    script ='ingestGtfs.R'
+    cmd = [rscript_command, pipeline_dir + script]
+    subprocess.run(cmd)
+    
+    # STEP 2 - CREATE BLOCKS, DEAD TRIP & DEAD LEG INFO
+    # =================================================
+    # Build & run subprocess command
+    # WARNING --- This processing script takes sevral hours to run.
+    script ='createBlockInfo.R'
+    cmd = [rscript_command, pipeline_dir + script]
+    subprocess.run(cmd)
+    
+    # STEP 3 - GET RAW ROUTE INFO FOR DEAD LEGS & DEAD TRIPS
+    # ======================================================
+    import ingestNonGtfsRoutes
+    
+    def service_func_ingr():
+        print('Running ingest non-GTFS route script...')
+    
+    if __name__ == '__main__':
+        # if this scipt is executed as script, run:
+        service_func_ingr()
+        ingestNonGtfsRoutes.run_all_ingr()
+    
+    # STEP 4 - EXTRACT RAW ROUTE DATA & TRANSFORM
+    # ======================================================
+    import extractTransformLoadRoutes
+    
+    def service_func_etlr():
+        print('Running extract & transfrom non-GTFS script...')
+    
+    if __name__ == '__main__':
+        # if this scipt is executed as script, run:
+        service_func_etlr()
+        extractTransformLoadRoutes.run_all_etlr()    
+    
+    # STEP 5 - CREATE NETWORK SUMMARY INFO
+    # ====================================
+    # Build & run subprocess command
+    # WARNING --- This processing script takes sevral hours to run.
+    script ='createBlockSummary.R'
+    cmd = [rscript_command, pipeline_dir + script]
+    subprocess.run(cmd)
+    
+finally :
+    # return to root directory
+    os.chdir('C:\MyApps\dapTbElectricDublinBus')
 

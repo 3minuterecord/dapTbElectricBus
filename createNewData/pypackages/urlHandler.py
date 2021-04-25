3c93c1663429402f1899077b3becdc9b5b729a9c
James Cookland <x20167814@student.ncirl.ie>
2021-04-25 08:43:40 +0100

    Reduce get request

--- createNewData/pypackages/urlHandler.py
+++ createNewData/pypackages/urlHandler.py
@@ -1,6 +1,9 @@
 import urllib
 import urllib.request
 import json
+from math import sqrt
+from geopy.distance import great_circle
+from geopy.distance import geodesic
 
 from numpy import empty
 import createNewData.data.config as in_config
@@ -17,6 +20,10 @@ class UrlHandler():
             return self.callURL(args[1], args[2], args[3])
         elif args[0] == "generateLocationRequest":
             return self.generateLocationRequest(args[1])
+        elif args[0] == "EuclideanDist":
+            return self.EuclideanDist(args[1], args[2], 
+                                      args[3],args[4], 
+                                      args[5], args[6])
         else:
             return "Object does not exist."
 
@@ -25,7 +32,7 @@ class UrlHandler():
             req = urllib.request.Request(url, body, headers)
         else:
             req = urllib.request.Request(url, headers=headers)
-        response = urllib.request.urlopen(req)
+        response = urllib.request.urlopen(req, timeout=500000)
         return response
         
     def generateLocationRequest(self, shapeData):
@@ -46,3 +53,13 @@ class UrlHandler():
         jsonReadyData = response.read().decode('utf8').replace("'", '"')
         elevationData = json.loads(jsonReadyData)
         return elevationData
+
+    def EuclideanDist(self, alt1, alt2, lon1, lat1, lon2, lat2):
+        alt_1 = alt1
+        alt_2 = alt2
+        dalt = alt_1-alt_2
+        p1 = (lon1, lat1)
+        p2 = (lon2, lat2)
+        calt = geodesic(p1, p2).meters
+        trueDistance = sqrt(calt**2 + dalt**2)
+        return trueDistance
